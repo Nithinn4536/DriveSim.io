7a067676014c4c50ed288792ed0153d3adb737d1
Nithinn4536 <knithin4536@gmail.com>
2025-08-09 20:35:26 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Driving Simulator</h1>
+      <p>Welcome to the driving simulator game!</p>
+    </div>
+  );
+}
